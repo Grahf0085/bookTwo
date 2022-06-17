d9e115b009747cc2f0ac7c9a903d97bf468a24e2
Tucker <tuckerhoog@tutanota.com>
2022-06-17 11:10:07 -0400

    initial tailwind commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import styles from './App.module.css';
+import logo from './logo.svg'
+import styles from './App.module.css'
 
 function App() {
   return (
     <div class={styles.App}>
       <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
+        <img src={logo} class={styles.logo} alt='logo' />
+        <p class='text-4xl text-red-400 tracking-widest'>
           Edit <code>src/App.jsx</code> and save to reload.
         </p>
+        <p class='text-4xl text-red-400 tracking-widest'>Test</p>
         <a
           class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
+          href='https://github.com/solidjs/solid'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn Solid
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
