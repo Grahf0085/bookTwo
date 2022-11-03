93881740a62908cf726a39127faae7f08eeac30c
Tucker <tuckerhoog@tutanota.com>
2022-11-03 15:10:03 -0400

    add suspense to chapter IOT stop paragraphs from being cut off

--- src/book-parts/Chapters.jsx
+++ src/book-parts/Chapters.jsx
@@ -1,4 +1,4 @@
-import { createResource, For } from 'solid-js'
+import { createResource, For, Suspense } from 'solid-js'
 import { fetchBookChapters } from '../utils/nietzscheAPI.js'
 import { Paragraphs } from './Paragraphs.jsx'
 
@@ -9,20 +9,22 @@ export const Chapters = (props) => {
   )
 
   return (
-    <For each={fetchedBookChapters()}>
-      {(chapter) => (
-        <>
-          <h2 id={chapter.chapterNumber} class='px-20 font-semibold'>
-            {chapter.chapterName}
-          </h2>
-          <Paragraphs
-            chapterNumber={chapter.chapterNumber}
-            title={props.title}
-            translator={props.translator}
-          />
-          <div class='h-full' />
-        </>
-      )}
-    </For>
+    <Suspense fallback={<p>Loading...</p>}>
+      <For each={fetchedBookChapters()}>
+        {(chapter) => (
+          <>
+            <h2 id={chapter.chapterNumber} class='px-20 font-semibold'>
+              {chapter.chapterName}
+            </h2>
+            <Paragraphs
+              chapterNumber={chapter.chapterNumber}
+              title={props.title}
+              translator={props.translator}
+            />
+            <div class='h-full' />
+          </>
+        )}
+      </For>
+    </Suspense>
   )
 }
