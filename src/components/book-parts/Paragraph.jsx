8e4c3d7085b8634a93eadfd503b6b3187144fbdd
Tucker <tuckerhoog@tutanota.com>
2023-01-08 17:22:17 -0500

    basic functionality for see also - not all info displayed in see also section

--- src/components/book-parts/Paragraph.jsx
+++ src/components/book-parts/Paragraph.jsx
@@ -1,6 +1,6 @@
 //dynamically adding span into <p> element at right place implemented with much appreciated assistance from https://github.com/0kku
 
-import { For } from 'solid-js'
+import { createSignal, For, Show } from 'solid-js'
 import { createSetVisibleParagraphs } from '../../providers/ParagraphProviders.jsx'
 import { NoteSpan } from './NoteSpan.jsx'
 import { SeeAlso } from './SeeAlso.jsx'
@@ -8,42 +8,60 @@ import { SeeAlso } from './SeeAlso.jsx'
 export const Paragraph = (props) => {
   let footnoteCount = -1
 
+  const [seeAlsoText, setAlsoText] = createSignal('')
+
   const setVisibleParagraphs = createSetVisibleParagraphs()
 
   return (
-    <p
-      ref={(el) => setVisibleParagraphs((p) => [...p, el])}
-      class='whitespace-pre-wrap overflow-scroll w-full px-20 py-2 overflow-x-hidden'
-    >
-      <For each={props.text}>
-        {(word) => {
-          if (word.match(/.+[⁰¹²³⁴⁵⁶⁷⁸⁹]+/gu)) {
-            let endIndex
-            for (let value of word) {
-              if (value.match(/[⁰¹²³⁴⁵⁶⁷⁸⁹]/))
-                endIndex = word.lastIndexOf(value)
+    <div class='h-min overflow-x-hidden flex'>
+      <p
+        ref={(el) => setVisibleParagraphs((p) => [...p, el])}
+        class={`whitespace-pre-wrap overflow-scroll h-full px-20 py-2 overflow-x-hidden ${
+          seeAlsoText() ? 'w-2/3' : 'w-full'
+        }`}
+      >
+        <For each={props.text}>
+          {(word) => {
+            if (word.match(/.+[⁰¹²³⁴⁵⁶⁷⁸⁹]+/gu)) {
+              let endIndex
+              for (let value of word) {
+                if (value.match(/[⁰¹²³⁴⁵⁶⁷⁸⁹]/))
+                  endIndex = word.lastIndexOf(value)
+              }
+              const wordWithFootnote = word.substring(0, endIndex + 1)
+              const restOfWord = word.substring(endIndex + 1)
+              footnoteCount++
+              return (
+                <NoteSpan
+                  footnoteCount={footnoteCount}
+                  word={wordWithFootnote}
+                  restOfWord={restOfWord}
+                  paragraphNumber={props.paragraphNumber}
+                  chapterNumber={props.chapterNumber}
+                  title={props.title}
+                  translator={props.translator}
+                />
+              )
             }
-            const wordWithFootnote = word.substring(0, endIndex + 1)
-            const restOfWord = word.substring(endIndex + 1)
-            footnoteCount++
-            return (
-              <NoteSpan
-                footnoteCount={footnoteCount}
-                word={wordWithFootnote}
-                restOfWord={restOfWord}
-                paragraphNumber={props.paragraphNumber}
-                chapterNumber={props.chapterNumber}
-                title={props.title}
-                translator={props.translator}
-              />
-            )
-          }
-          return word + ' '
-        }}
-      </For>
-      <For each={props.seeAlsoArray} fallback={<></>}>
-        {(seeAlso) => <SeeAlso seeAlso={seeAlso} />}
-      </For>
-    </p>
+            return word + ' '
+          }}
+        </For>
+        <For each={props.seeAlsoArray} fallback={<></>}>
+          {(seeAlso) => (
+            <SeeAlso
+              seeAlso={seeAlso}
+              seeAlsoText={seeAlsoText()}
+              setAlsoText={setAlsoText}
+            />
+          )}
+        </For>
+      </p>
+      <Show when={seeAlsoText() !== ''}>
+        <div class='w-1/3'>
+          <button onClick={() => setAlsoText('')}>Close</button>
+          <p>{seeAlsoText()}</p>
+        </div>
+      </Show>
+    </div>
   )
 }
--- src/components/book-parts/SeeAlso.jsx
+++ src/components/book-parts/SeeAlso.jsx
@@ -1,25 +1,28 @@
-import { createSignal, createResource, Show } from 'solid-js'
+//TODO translator(add options to switch translator)
+
+import { createResource, Show } from 'solid-js'
 import { fetchSeeAlso } from '../../utils/nietzscheAPI.js'
-/* import { FullText } from './FullText.jsx' */
 
 export const SeeAlso = (props) => {
-  const [showSeeAlso, setShowSeeAlso] = createSignal(false)
-
   const [fetchedSeeAlso] = createResource(() => props.seeAlso, fetchSeeAlso)
 
   return (
-    <>
-      <Show when={fetchedSeeAlso()} fallback={<></>}>
-        <h3
-          onClick={() => setShowSeeAlso(!showSeeAlso())}
-          class='cursor-pointer'
-        >
-          See Also: {fetchedSeeAlso().title} translated by{' '}
-          {fetchedSeeAlso().translatorName} chapter{' '}
-          {fetchedSeeAlso().chapterNumber} paragraph{' '}
-          {fetchedSeeAlso().paragraphNumber}
-        </h3>
-      </Show>
-    </>
+    <Show when={fetchedSeeAlso()} fallback={<></>}>
+      <h3
+        onClick={() => {
+          props.setAlsoText(
+            props.seeAlsoText === fetchedSeeAlso().paragraphText
+              ? ''
+              : fetchedSeeAlso().paragraphText
+          )
+        }}
+        class='cursor-pointer'
+      >
+        <br />
+        See Also: {fetchedSeeAlso().title} chapter{' '}
+        {fetchedSeeAlso().chapterNumber} paragraph{' '}
+        {fetchedSeeAlso().paragraphNumber}
+      </h3>
+    </Show>
   )
 }
